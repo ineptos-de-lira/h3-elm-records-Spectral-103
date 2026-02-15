c041f8d24b18c104a85e8e265695e0be04cbbb51
Spectral-103 <up240712@alumnos.upa.edu.mx>
2026-02-15 14:28:43 -0600

    First commit

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,126 @@
 module Helper exposing (..)
+
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+
+-- Ejercicio 1
+
+
+langs : List { name : String, releaseYear : Int, currentVersion : String }
+langs =
+    [ { name = "elm"
+      , releaseYear = 2012
+      , currentVersion = "0.19.1"
+      }
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAScript 2025"
+      }
+    ]
+
+
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+languageNames list =
+    List.map .name list
+
+
+
+-- Ejercicio 2
+
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+users : List User
+users =
+    [ { name = "Luis", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents lista =
+    List.map
+        (\user ->
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+        )
+        lista
+
+
+
+-- Ejercicio 3
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+videogames : List Videogame
+videogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 1234567
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of time"
+      , releaseYear = 1998
+      , available = False
+      , downloads = 12345
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres listaDeJuegos =
+    List.map .genres listaDeJuegos
+
+
+
+-- Html
+-- Ejercicio HTML
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "P15"
+    , brand = "Huawei"
+    , screenSize = "15 pulgadas"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
